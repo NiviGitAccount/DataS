8625cd1013744ab51c60b0a353c07988a41b44a6
rudy <snivedha6@gmail.com>
2021-06-18 16:16:22 +0530

    is-unique-string

---
+++ Arrays_And_Strings/is-unique-string.js
@@ -0,0 +1,39 @@
+//usingPredefinedMethod  with Time complexity O(n^2)
+function usingPredefinedMethod(stringValue){
+    for(var i=0;i<stringValue.length;i++){
+        if(stringValue.indexOf(stringValue[i]) !== stringValue.lastIndexOf(stringValue[i])){
+            return `Given string ${stringValue} is not unique`;
+        }
+    }
+    return `Given string ${stringValue} is unique`;
+}
+
+//BruteForce technique with Time complexity O(n^2)
+
+function usingBFMethod(stringValue){
+     
+    for(let i = 0; i < stringValue.length; i++)
+        for(let j = i + 1; j < stringValue.length; j++)
+            if (stringValue[i] == stringValue[j])
+                return `Given string ${stringValue} is not unique`;
+ 
+    return `Given string ${stringValue} is unique`;
+}
+ 
+//Sorting nlog(n)
+function usingSorting(stringConstant){
+    let stringArr = Array.from(stringConstant);
+    stringArr.sort();
+
+    for(let i = 0; i < stringArr.length; i++){
+        if(stringArr[i] === stringArr[i+1])
+        return `Given string ${stringConstant} is not unique`;
+    }
+
+    return `Given string ${stringConstant} is unique`;
+}
+
+
+console.log(usingBFMethod("Geeks"));
+console.log(usingPredefinedMethod("Hellllo"));
+console.log(usingSorting("HellooWorld"));
